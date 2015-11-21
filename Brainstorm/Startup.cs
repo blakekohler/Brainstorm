3d3a6bf2205938137ae42f1e3c7ddf97fb4f8ee1
Blake Kohler <blakekohler@Blake-Kohlers-MacBook-Air.local>
2015-11-21 11:05:52 -0700

    Initial commit

---
+++ Brainstorm/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Brainstorm.Startup))]
+namespace Brainstorm
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
